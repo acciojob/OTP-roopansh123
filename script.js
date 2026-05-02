d282eff0774df2a5df9c5d27b93e06e55242266a
Roopansh Sethi <38037190+roopansh123@users.noreply.github.com>
2026-05-02 18:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+	code.addEventListener("input", (e) => {
+		if (e.target.value && index < codes.length - 1) {
+			codes[index + 1].focus();
+		}
+	});
+
+	code.addEventListener("keydown", (e) => {
+		if (e.key === "Backspace") {
+			if (code.value === "" && index > 0) {
+				codes[index - 1].focus();
+			}
+		}
+	});
+});
